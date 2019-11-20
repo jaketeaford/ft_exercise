f86afdd0f7b1dad497755e56f358d2fbec0fe696
Jake Teaford <jaketeaford@gmail.com>
2019-11-20 13:44:57 -0800

    photo upload functionality

--- realtor/admin.py
+++ realtor/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from realtor.models import House
+from realtor.models import House, HousePhoto
 
 # Register your models here.
-admin.site.register(House)
+class HousePhotoAdmin(admin.StackedInline):
+	model = HousePhoto
+	fields = ('image_tag', 'photo')
+	readonly_fields = ['image_tag']
+
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+	inlines = [HousePhotoAdmin]
+
+@admin.register(HousePhoto)
+class HousePhotoAdmin(admin.ModelAdmin):
+	pass
--- realtor/models.py
+++ realtor/models.py
@@ -1,19 +1,36 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+
+GARAGES = (
+	('No Garage', 'No Garage'),
+	('1-Car Garage', '1-Car Garage'),
+	('2-Car Garage', '2-Car Garage'),
+	('3-Car+ Garage', '3-Car+ Garage'),
+)
 
 # Create your models here.
 class House(models.Model):
-	mls_num = models.IntegerField(unique=True)
-	street1 = models.CharField(max_length=100)
-	street2 = models.CharField(max_length=50)
+	mls_num = models.IntegerField(unique=True, verbose_name="MLS Number")
+	street1 = models.CharField(max_length=100, verbose_name="Address Line 1")
+	street2 = models.CharField(max_length=50, verbose_name="Address Line 2", blank=True)
 	city = models.CharField(max_length=100)
 	state = models.CharField(max_length=25)
-	zip_code = models.IntegerField()
+	zip_code = models.IntegerField(verbose_name="Zip Code")
 	neighborhood = models.CharField(max_length=100)
-	sales_price = models.IntegerField()
-	list_date = models.DateField()
-	bed_count = models.IntegerField()
-	bath_count = models.IntegerField()
-	garage_size = models.CharField(default="No Garage", max_length=20)
-	sq_ft = models.IntegerField()
-	lot_size = models.IntegerField()
+	sales_price = models.IntegerField(verbose_name="Sales Price")
+	list_date = models.DateField(verbose_name="Date Listed")
+	bed_count = models.IntegerField(verbose_name="Bedrooms")
+	bath_count = models.IntegerField(verbose_name="Bathrooms")
+	garage_size = models.CharField(default="No Garage", max_length=20, choices=GARAGES)
+	sq_ft = models.IntegerField(verbose_name="Square Footage")
+	lot_size = models.IntegerField(verbose_name="Lot Size (Acres)")
 	description = models.TextField()
+
+class HousePhoto(models.Model):
+	house = models.ForeignKey(House, on_delete=models.CASCADE, null=True)
+	photo = models.ImageField()
+
+	def image_tag(self):
+		return mark_safe('<img src="%s" height="200" />' % (self.photo.url))
+
+	image_tag.short_description = 'Image'
